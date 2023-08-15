126460ed5a69cb0b9182030978b1f35ebe175c02
joao-paranhos <140024637+joao-paranhos@users.noreply.github.com>
2023-08-15 17:50:17 -0300

    inserindo dados na tabela clientes

---
+++ INSERT DA TABELA CLIENTES.sql
@@ -0,0 +1,24 @@
+INSERT INTO CLIENTES
+
+VALUES
+
+('64dbcafcd70e4','Solange Menezes Ferraz','09352008006','Rua Estella Andrade dos Santos 153','8122772322','97992243598','maciana7508@uorak.com'),
+('64dbd199481af','Cícera Dominato Garcia','12290184098','Quadra QS 127 Conjunto F','4133689949','99984357479','millerlandy5666@uorak.com'),
+('64dbd262e1f54','Cláudia Ávila','96171888052','Rua Leandro Francisco da Silva','8636947475','99984357479','lambert107@uorak.com'),
+('64dbd488226f8','Ashley Duarte Espada','36259110006','Rua Imperial','8338056087','99984357479','nataly1813@uorak.com'),
+('64dbd52f4d3cb','Amanda Rosimeire Colaço de Domingues','36259110006','Rua Monte Branco','8421341236','99984357479','outman2558@uorak.com'),
+('64dbd5a13bb3e','Pablo Batista Castelo Branco','29135444606','Rua Aquário 65','6135944917','99984357479','bernetta9277@uorak.com'),
+('64dbd6505b6e7','Ângelo Burgos Filho','29135444606','Quadra QN 5 Conjunto 14','6721257495','99984357479','babak6993@uorak.com'),
+('64dbd6d4a7305','Amélia Graziele Chaves Valentin de Cardozo','29135444606','Rua Antônio Dias Sobreira','6721257495','99984357479','habibou4030@uorak.com'),
+('64dbd7197edb3','Noel Robson Barros Padrão','29135444606','Rua Antônio Dias Sobreira','6721257495','99984357479','sonnia698@uorak.com'),
+('64dbd76b0af23','Carolina Mônica Rodrigues de Teles','29135444606','Travessa Faria da Costa','6721257495','99984357479','sonnia698@uorak.com'),
+('64dbd8c20d1f0','Clotilde Feliciano Ramos','29135444606','Rua Pedro Cardônio Uchôa','8524118190','83993887613','kaylene5337@uorak.com'),
+('64dbd9220561b','Alan Isaías Dominato','36346442228','Estrada Flor de Liz','6727386244','86993665914','montsant3629@uorak.com'),
+('64dbd9e90503d','Elisângela Maria Faria','03212406267','Rua José Carlos Medina','9925128971','86993665914','maddie2538@uorak.com'),
+('64dbdaaf7704d','Ivan Burgos de Ortega','98229377332','Praça Nossa Senhora da Conceição','9325869351','65987901220','aichata1525@uorak.com'),
+('64dbdaf335aee','Nicole Valquíria Carmona Corona de Chaves','53113522641','Rua Aguanaval','8323174425','87974731770','guzman3497@uorak.com'),
+('64dbdb8380bd3','Mariana Corona','18146960677','Quadra Quinze 51','6933228682','88998949434','soumeya7193@uorak.com'),
+('64dbdbc855cb0','Bárbara Faro Milani','18146960677','Rua Rio do Meio','6933228682','79979052135','germana5304@uorak.com'),
+('64dbdc033b81b','Ezequiel João Campos','88508095309','Vila Rosa Pinheiro','8134539136','49974892086','boubaker7535@uorak.com'),
+('64dbdc42cbfca','Sara Eulália Barros Martines','38185110204','Rua Pedro Moura','1226258057','83991935962','atef2503@uorak.com')
+
