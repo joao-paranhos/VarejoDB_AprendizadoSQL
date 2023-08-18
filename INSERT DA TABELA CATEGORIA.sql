b435373e9c9b7e2e8aa51d69a95aa4373a2f6c7a
joao-paranhos <140024637+joao-paranhos@users.noreply.github.com>
2023-08-18 20:57:50 -0300

    insert da tabela categoria

---
+++ INSERT DA TABELA CATEGORIA.sql
@@ -0,0 +1,25 @@
+-- Inserção de várias categorias na tabela CATEGORIA
+
+-- Inserção das categorias
+INSERT INTO CATEGORIA (CATEGORIA_ID, NOME)
+VALUES
+    ('64dffc572ac75', 'Eletrônicos'),                             
+    ('64dffca807cff', 'Vestuário Masculino'),                     
+    ('64dffced9e795', 'Vestuário Feminino'),                      
+    ('64dffd1ceae15', 'Casa e Decoração'),                        
+    ('64dffd270dd00', 'Alimentos e Bebidas'),                    
+    ('64dffd2dba770', 'Beleza e Cuidados Pessoais'),               
+    ('64dffd358e3f2', 'Esportes e Lazer'),                        
+    ('64dffd3ab5aa4', 'Brinquedos, jogos de tabuleiros, quebra-cabeças'), 
+    ('64dffd41e555f', 'Livros, filmes, músicas, jogos'),          
+    ('64dffd49d07de', 'Móveis para casa e escritórios'),         
+    ('64dffd4f8db58', 'Acessórios com bolsas, óculos, relógios, jóias'),
+    ('64dffd5d08d2c', 'Saúde e Bem-Estar'),                    
+    ('64dffd660a97f', 'Automotivo'),                              
+    ('64dffd6cdc18c', 'Artigos para Festas'),                     
+    ('64dffd73d4526', 'Eletrônicos para Entretenimento'),        
+    ('64dffd7a8e278', 'Ferramentas e equipamentos'),              
+    ('64dffd804d91d', 'Papelaria e Escritório'),                  
+    ('64dffd86416f1', 'Jardinagens e Exterior'),                 
+    ('64dffd9224a3e', 'Viagens e Aventura'),                      
+    ('64dfff7a118ad', 'Arte e Decoração');                        
