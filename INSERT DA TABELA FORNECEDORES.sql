7ab4f0795c694223ee8b80d50387089f16744c20
joao-paranhos <140024637+joao-paranhos@users.noreply.github.com>
2023-10-06 10:33:51 -0300

    INSERT TABELA FORNECEDORES

---
+++ INSERT DA TABELA FORNECEDORES.sql
@@ -0,0 +1,16 @@
+INSERT INTO FORNECEDORES
+
+VALUES
+
+('64e016d09129f','Eletrônicos Inc.','Rua Rio Bonito 23','SP','1138243271','11974927796','bogna5724@uorak.com'),
+('64e007cd6bff5','FashionSuppliers','Rua Maria de Lourdes Abrantes Pinto de Oliveira 564','MG','3133183613','31981124683','cancio5176@uorak.com'),
+('64e007d8e3098','DecoraçãoMais','Rua Elizio Dorneles 63','RJ','2424170862','24969225963','ofelio3573@uorak.com'),
+('64e007df39e25','FornecAlimentos','Rua Capitão Newton Rubem 1051','RJ','2237775136','22971455368','eduviges4820@uorak.com'),
+('64e007e420223','SaúdeSuprimentos','Rua Francisco Alves de Souza 342','AM','9738360248','97994751452','eszter4331@uorak.com'),
+('64e007e895180','SportsEquip','Rua Martiniano Correia 555 ','SP','1121266251','11981846175','maryellen451@uorak.com'),
+('64e007ee712b3',' BookWorld Distribuição','Travessa Raul Pompéia 452','SP','1137616051','11969574861','elvita932@uorak.com'),
+('64e007f38cdfa','MobíliaSuprimentos',' Rua da Garoupa 13','SP','1138251862','11989094181','calogero9859@uorak.com')
+
+
+ALTER TABLE FORNECEDORES ALTER COLUMN TELEFONE VARCHAR (10)
+ALTER TABLE FORNECEDORES ALTER COLUMN CELULAR VARCHAR (11)
