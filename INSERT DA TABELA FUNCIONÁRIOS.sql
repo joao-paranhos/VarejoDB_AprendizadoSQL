7d8881800b88f98a8f15d0fe519c9edeb89308aa
joao-paranhos <140024637+joao-paranhos@users.noreply.github.com>
2023-10-06 09:34:45 -0300

    INSERT DA TABELA FUNCIONÁRIOS

---
+++ INSERT DA TABELA FUNCIONÁRIOS.sql
@@ -0,0 +1,25 @@
+
+
+INSERT INTO FUNCIONARIOS
+
+VALUES 
+
+('6509e53b2f273','Ayrton Fernandes','Vendedor','2021-09-22','2500'),
+('6509e5a8ed031','Juliana Flaviana','Vendedor','2022-05-21','2000'),
+('6509e613f082f','Leandro Ronaldo Dias de Braga','Vendedor','2020-01-01','1500'),
+('6509e659c00e8','César Bonilha','Gerente','2019-02-21','5000'),
+('6509e6e5ab6aa','Baltazar Galvão de Alencar','Estoquista','2023-05-20','2100'),
+('651ff92da4f57','Edu Chaves Filho','Gerente de RH','2023-05-20','5000'),
+('651ff9fbc8435','Eloá Yasmin Meireles de Lemos','Analista de Marketing','2019-03-21','3100'),
+('651ffa428f49b','Fabiana Maria de Delgado','Analista de RH','','3100'),
+('651ffab46c25b','Filipe Queirós Brasil','Gerente Comercial','2020-10-22','5100'),
+('651ffadb81404','Fabiana Leal Marinho','Vendedor','2023-10-01','2300'),
+('651ffb48cd728','Mel Luciana Branco Queirós','TI','2023-10-02','2500'),
+('651ffb9fd1343','Gustavo Wesley Esteves Reis','TI','2019-10-02','3100'),
+('651ffbbc2d6c7','Filipe Queirós Brasil','Caixa','2020-10-02','1800'),
+('651ffc4483247','Taís Ingrid de Camacho','Caixa','2019-01-12','2100'),
+('651ffc845c699','Wanda Xilena Cervantes de Meireles','Caixa','2021-01-01','1900')
+
+select * from FUNCIONARIOS
+
+
